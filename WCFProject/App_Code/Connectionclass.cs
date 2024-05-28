0b00b9fec74ac282ab2152ee3a3428388931defd
monikkasekhar <monikkasekhar@gmail.com>
2024-05-28 12:42:21 +0530

    Add project files.

---
+++ WCFProject/App_Code/Connectionclass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+
+/// <summary>
+/// Summary description for Connectionclass
+/// </summary>
+public class Connectionclass
+{
+    SqlConnection con;
+    SqlCommand cmd;
+    public Connectionclass()
+    {
+        con = new SqlConnection(@"server=MSI\SQLEXPRESS;database=DB_Project;Integrated security=true");
+    }
+
+    public SqlDataReader Fn_ExeReader(string s)
+    {
+        if(con.State==ConnectionState.Open)
+        {
+            con.Close();
+        }
+        cmd = new SqlCommand(s,con);
+        con.Open();
+        SqlDataReader dr = cmd.ExecuteReader();
+        return dr;
+    }
+}
---
+++ WCFProject/App_Code/Service.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+	Connectionclass obj = new Connectionclass();
+
+	public string balancecheck(string accno)
+    {
+		string sel = "select Balance_Amount from Account_Tab where Account_Number='"+accno+"'";
+		SqlDataReader dr = obj.Fn_ExeReader(sel);
+		string bal = "";
+		while(dr.Read())
+        {
+			bal = dr["Balance_Amount"].ToString();
+
+        }
+		return bal;
+    }
+	public string GetData(int value)
+	{
+		return string.Format("You entered: {0}", value);
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException("composite");
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+}
